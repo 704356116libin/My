15d976eb57874b9c7bda63e522c0cb73e1c8ad43
704356116libin <704356116@qq.com>
2018-12-24 21:08:19 +0800

    扯淡的ssh

--- src/main/java/bin/Http/Controllers/DemoController.java
+++ src/main/java/bin/Http/Controllers/DemoController.java
@@ -1,20 +1,20 @@
-//package bin.Http.Controllers;
-//
-//import com.opensymphony.xwork2.ActionSupport;
-//import org.apache.struts2.convention.annotation.*;
-//
-//@ParentPackage("base")
-//@Namespace(value = "/demo")
-//@Results({
-//        @Result(name="6666",location = "/index.jsp"),
-//})
-//public class DemoController extends ActionSupport{
-//    public DemoController() {
-//        System.out.println("demo 初始化!!!!!!!!!!!!!!!!!111");
-//    }
-//
-//    @Action("/hello")
-//    public String demo(){
-//       return "6asd666";
-//     }
-//}
+package bin.Http.Controllers;
+
+import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.convention.annotation.*;
+
+@ParentPackage("base")
+@Namespace(value = "/demo")
+@Results({
+        @Result(name="6666",location = "/index.jsp"),
+})
+public class DemoController extends ActionSupport{
+    public DemoController() {
+        System.out.println("demo 初始化!!!!!!!!!!!!!!!!!111");
+    }
+
+    @Action("/hello")
+    public String demo(){
+       return "6asd666";
+     }
+}
